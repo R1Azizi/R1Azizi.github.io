4baff05c62c948bf31296e0cb6dc4747fb1eaf91
R1Azizi <arvanazizi1234@gmail.com>
2023-07-28 04:16:00 +0430

    hhee

--- code.js
+++ code.js
@@ -14,3 +14,22 @@ function submitData() {
     const divisorsList = findDivisors(inputValue);
     document.getElementById("output").innerText = "شمارنده های عدد : " +divisorsList;
     }
+
+
+    const webhookUrl = 'https://discord.com/api/webhooks/1134269345388843029/Va9H6impfXsET583Am58BwTGjCaVRKbY_UK1ZHx3mYNHj7_g1wsfy4FEevyyH2v1xEUI';
+function sendMessageToWebhook(message) {
+  fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content: message }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error sending the message.');
+    }
+  })
+}
+
+sendMessageToWebhook('Someone just entered r1azizi.github.io');
