c87ac74e95d9d6b4880caa25f2431a8a8d2ab749
R1Azizi <arvanazizi1234@gmail.com>
2023-07-28 03:46:12 +0430

    done

---
+++ Oath/code.js
@@ -0,0 +1,16 @@
+function findDivisors(number) {
+    const divisors = [];
+    for (let i = 1; i <= number; i++) {
+      if (number % i === 0) {
+        divisors.push(i);
+      }
+    }
+    return divisors;
+  }
+  
+function submitData() {
+    const numberInput = document.getElementById("numberInput");
+    const inputValue = numberInput.value;
+    const divisorsList = findDivisors(inputValue);
+    document.getElementById("output").innerText = divisorsList;
+    }
