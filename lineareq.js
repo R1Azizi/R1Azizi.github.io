d00bd2822a5301a106be54e247e728453b225e50
R1Azizi <arvanazizi1234@gmail.com>
2024-04-13 01:02:27 +0430

    tidy

--- lineareq.js
+++ lineareq.js
@@ -7,16 +7,13 @@ function updateEquation() {
     const a = parseFloat(inputA.value);
     const b = parseFloat(inputB.value);
 
-    // Check if the parsed values are valid numbers
     if (!isNaN(a) && !isNaN(b)) {
         equationText.textContent = `y = ${a}x + ${b}`;
         
-        // Calculate coordinates
         const xCoordinate1 = 0;
         const yCoordinate1 = b;
         const xCoordinate2 = (b === 0) ? 0 : -b/a; // Solve for x when y = 0
 
-        // Update coordinates text
         coordinatesText.innerHTML = `[ x: ${xCoordinate1}, y: ${yCoordinate1} ] : طول از مبدا <br>[ x: ${xCoordinate2}, y: 0 ] : عرض از مبدا `;
     } else {
         equationText.textContent = "y = ax + b";
