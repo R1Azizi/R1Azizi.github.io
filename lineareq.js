80878448c36ae771741a4552a6b9d4f07d8fd769
R1Azizi <arvanazizi1234@gmail.com>
2024-04-20 00:04:39 +0430

    added x and y input

--- lineareq.js
+++ lineareq.js
@@ -1,6 +1,8 @@
 const inputA = document.getElementById('inputA');
 const inputB = document.getElementById('inputB');
 const inputY = document.getElementById('inputY');
+const radioX = document.getElementById('radioX');
+const radioY = document.getElementById('radioY');
 const equationText = document.getElementById('equation');
 const coordinatesText = document.getElementById('coordinates');
 
@@ -17,20 +19,49 @@ function updateEquation() {
         const xCoordinate2 = (b === 0) ? 0 : -b/a; // Solve for x when y = 0
 
         coordinatesText.innerHTML = `[ x: ${xCoordinate1}, y: ${yCoordinate1} ] : طول از مبدا <br>[ x: ${xCoordinate2}, y: 0 ] : عرض از مبدا `;
+
     } else {
         equationText.textContent = "y = ax + b";
         coordinatesText.innerHTML = "[ x: 0, y: 0 ] : طول از مبدا <br>[ x: 0, y: 0 ] : عرض از مبدا ";
     }
 
-    if (!isNaN(y)) {
+    if (!isNaN(y) && radioY.checked) {
         equationText.textContent = equationText.textContent.replace(/y/g, y);
         
         // Solve for x when y is given
         const x = (!isNaN(a) && a !== 0) ? (y - b) / a : "Undefined";
         coordinatesText.innerHTML += `<br>x = ${x}`;
     }
+
+    if (!isNaN(parseFloat(inputY.value)) && radioX.checked) {
+        const newX = !isNaN(a) ? a * parseFloat(inputY.value) : "Undefined";
+        equationText.textContent = equationText.textContent.replace(/(\d+)x/g, newX);
+        const newpX = parseFloat(inputY.value);
+        const newY = a * newpX + b;
+        coordinatesText.innerHTML += `<br>y = ${newY}`;
+    }
+    
 }
+
 
 inputA.addEventListener('input', updateEquation);
 inputB.addEventListener('input', updateEquation);
 inputY.addEventListener('input', updateEquation);
+
+radioX.addEventListener('change', function() {
+    if (radioX.checked) {
+        inputY.value = "";
+        coordinatesText.innerHTML = "[ x: 0, y: 0 ] : طول از مبدا <br>[ x: 0, y: 0 ] : عرض از مبدا ";
+        updateEquation();
+    }
+});
+
+radioY.addEventListener('change', function() {
+    if (radioY.checked) {
+        inputY.value = "";
+        equationText.textContent = "y = ax + b";
+        updateEquation();
+    }
+});
+
+
